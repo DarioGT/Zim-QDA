f44a0d70a1973c9b231df468e1514ef8c584c40b
dario <dariogomezt@gmail.com>
2015-03-14 00:13:35 -0400

    Manejo de exportacion de jerarquia Ok

--- zim/plugins/qdacodes/qdaExportMapDoc.py
+++ zim/plugins/qdacodes/qdaExportMapDoc.py
@@ -9,6 +9,9 @@ from qdaSettings import sluglify
 from qdaSettings import NOTE_MARK, NOTE_AUTOTITLE
 
 TAG_MARK = ""
+SEPLISTA = '-'
+SEP__TAG = ';'
+SEP_CODE = ','
 
 
 class doQdaExportMapDoc(object):
@@ -23,129 +26,88 @@ class doQdaExportMapDoc(object):
 
         self.plugin = plugin
         self.ui = plugin.ui 
-        self.idPage = 20   #lookup_path  plugin.ui.page  
+        self.page = plugin.ui.page   
+        self.path = self.ui.notebook.index.lookup_path(self.page )
+        self.pageId = self.path.id    
 
 
     def do_ExportMapDoc(self):
-        """ Exporta el mapa del documento 
-
-        Por cada codigo genera las fuentes con formato record y label
-        Genera una lista de codigos con su label
-        genera la conexion de cada autor con los codigos
-        si existen jerarquias ( listas separadas por , ) las presenta encadendas
-        cuando hay conceptos jerarquizados no deberia vincularlos a la fuente
+        """ Exporta el mapa del documento  ( ver wiki )
         """
-
-        masterPath = self.plugin.preferences['namespace']
 
         # La idea es q sea por fuente en la idenxacion del documento                  
         sOrder = 'source, tag, description'
-        sWhere = 'tag <> \'{1}\' and source = {0}'.format( self.idPage, 'QDATITLE' )
+        sWhere = 'tag <> \'{1}\' and source = {0}'.format( self.pageId, 'QDATITLE' )
 
-        SEPLISTA = '-'
-        SEP__TAG = ';'
-        SEP_CODE = ','
-
-        mySource = ''
-        zPages = {}
+        pageName = self.path.basename
+        zPage = {  'tags' : [], 'links' : [] }
 
         for row in self.plugin.list_codes(parent=None, orderBy=sOrder, whereStmt=sWhere):
-            path = self.plugin.get_path(row)
-            if path is None: continue
-
-            # Break by Source ( normalmente 1 sola )
-            source = path.name 
-            if source != mySource:
-                mySource = source
-                myCode = ''
-
-                # Agrega solo el basename 
-                zPages[ mySource ] = { 'name' : sluglify( source ),  'tags' : [], 'links' : [] }
 
             # Elimina los comentarios 
-            code = row['description'].decode('utf-8').split(':')[0].strip() 
-            if len (code) == 0 or code == myCode: continue
-            myCode = code
+            myCode = row['description'].decode('utf-8').split(':')[0].strip() 
+            if len (myCode) == 0: continue
 
-            # No agrega el = ; es simplemente parte de la jerarquia 
-            tag = row['tag'].decode('utf-8').strip() 
+            # Lo guarda de Bk pues pueden venir varios ; en la linea 
+            tag0 = row['tag'].decode('utf-8').strip() 
 
             # Separa las relaciones directas al tag 
-            myLinksA = [ j.strip() for j in code.split(SEP__TAG) ]
-            for iTag  in range(0, len(myLinksA)):
-                linkA = myLinksA[ iTag ] 
+            myCodeA = [ j.strip() for j in myCode.split(SEP__TAG) ]
+            for i  in range(0, len(myCodeA)):
+                linkA = myCodeA[ i ] 
 
-                if iTag > 0: 
-                    tag = getTag( linkA )
-                    if len( tag ) > 1 : 
-                        tag = tag[1:] 
-                        linkA = linkA[len(tag)+1:].strip()
+                # Si no es la primera cadena 
+                if i > 0: 
+                    tag2 = getTag( linkA )
+                    if len( tag2 ) > 1 : 
+                        tag2 = tag2[1:].strip() 
+                        linkA = linkA[len(tag2)+1:].strip()
+                else: tag2 = tag0 
 
-                myTag = sluglify(tag)
-                if tag != '=' and not myTag in zPages[ mySource ][ 'tags'] :
-                    zPages[ mySource ][ 'tags'].append(myTag)
+                if len( tag2 ) == 0 : tag2 = tag0
+                myTag = sluglify(tag2)
 
-                # Separa los conceptos jerarquicos 
-                linkB = [ j.strip() for j in linkA.split(SEP_CODE) ]
-                for i in range(0, len(linkB)):
-                    code = linkB[ i ]
+                if tag2 != '=' and not myTag in zPage[ 'tags'] :
+                    zPage[ 'tags'].append(myTag)
 
-                    # Split a primer nivel Debe tener un tag 
-                    if SEPLISTA in code and i == 0  : 
-                        myLinksB = [ j.strip() for j in code.split(SEPLISTA) ] 
-                        for codeB in myLinksB :
-                            addMapLink( myTag, codeB, zPages[ mySource ] )
+                doCodeRelations( zPage, linkA, tag2, myTag )
 
-                    # Tiene q dividir el ultimo nodo 
-                    elif SEPLISTA in code and i == (len (linkB) - 1)   : 
-                        myLinksB = [ j.strip() for j in code.split(SEPLISTA) ] 
-                        codeA = linkB[ i - 1 ]
-                        for codeB in myLinksB :
-                            addMapLink( codeA, codeB, zPages[ mySource ] )
-
-                    # Primer nivel con tag 
-                    elif i == 0 and tag != '=': 
-                        addMapLink ( myTag , code, zPages[ mySource ] )
-
-                    # Primer nivel sin tag, no hace nada pues se hara con base en el tag anterior 
-                    elif i == 0 and tag == '=': 
-                        continue 
-
-                    # Caso normal de un link q debe encadenarse con el nivel anterior 
-                    elif i > 0  : 
-                        codeB = linkB[ i - 1 ]
-                        addMapLink ( codeB , code, zPages[ mySource ] )
+        doDotFile( self.path.basename, self.page.folder, zPage )
 
 
+def doCodeRelations( zPage, linkA, tag, myTag ):
 
-        # ============   Ya tiene la estructura de control 
-        pageName = 'xx' 
-#         prefixPage = 'xxx'
-#         pageName = ':{0}:{1}'.format(masterPath, prefixPage) 
-#         self.plugin.ui.append_text_to_page( pageName , '======== {0} =========\n\n'.format( prefixPage  ) )
+    # Separa los conceptos jerarquicos 
+    linkB = [ j.strip() for j in linkA.split(SEP_CODE) ]
+    for i in range(0, len(linkB)):
+        code = linkB[ i ]
 
+        # Split a primer nivel Debe tener un tag 
+        if SEPLISTA in code and i == 0  : 
+            myCodeB = [ j.strip() for j in code.split(SEPLISTA) ] 
+            for codeB in myCodeB :
+                addMapLink( myTag, codeB, zPage )
 
-#         masterPageIx = ''
-#         for zPage in zPages:
+        # Tiene q dividir el ultimo nodo 
+        elif SEPLISTA in code and i == (len (linkB) - 1)   : 
+            myCodeB = [ j.strip() for j in code.split(SEPLISTA) ] 
+            codeA = linkB[ i - 1 ]
+            for codeB in myCodeB :
+                addMapLink( codeA, codeB, zPage )
 
-#             masterPageIx += 'digraph {rankdir=LR\n\n//sources\n'
-#             masterPageIx += 'node [shape=box,shape=box, width=0, height=0, concentrate=true]\n\n'
-# #             masterPageIx += zPage( 'name' )
+        # Primer nivel con tag 
+        elif i == 0 and tag != '=': 
+            addMapLink ( myTag , code, zPage )
 
-#             masterPageIx += '//tags\nnode [style=rounded]\n\n'
+        # Primer nivel sin tag, no hace nada pues se hara con base en el tag anterior 
+        elif i == 0 and tag == '=': 
+            continue 
 
-#             for myTag in zPage['tags']:
-#                 masterPageIx += '\t{0} \t[label="{0}"]\n'.format( myTag, myTag )
+        # Caso normal de un link q debe encadenarse con el nivel anterior 
+        elif i > 0  : 
+            codeB = linkB[ i - 1 ]
+            addMapLink ( codeB , code, zPage )
 
-
-#             masterPageIx += '\n//QdaLinks\nnode [shape=oval,width=0, height=0, concentrate=true]\n'
-#             for myLink in zPage['links']:
-#                 masterPageIx += '\t{0}\n'.format( myLink  )
-
-#             masterPageIx += '}\n'
-
-        # get_attachments_dir
-        
 def addMapLink ( myCode1, myCode2, zPage   ): 
 
     myLink = '{0} -> {1}'.format( sluglify( myCode1 ), sluglify(myCode2))
@@ -164,3 +126,52 @@ def getTag(item):
 
     return  (item.split() or [''])[0].strip().upper()
 
+
+def doDotFile( pageName, folder, zPage ): 
+    """Creacion del archivo dot 
+    """
+
+    pageName = sluglify( pageName )
+
+    masterPageIx = 'digraph {rankdir=LR\n\n//sources\n'
+    masterPageIx += 'node [shape=box,shape=box, width=0, height=0, concentrate=true]\n\t'
+    masterPageIx +=  pageName 
+
+    masterPageIx += '\n\n//tags\nnode [style=rounded]\n\n'
+
+    for myTag in zPage['tags']:
+        masterPageIx += '\t{0} -> {1}\n'.format( pageName, myTag )
+
+    masterPageIx += '\n\n//QdaLinks\nnode [shape=oval,width=0, height=0, concentrate=true]\n'
+    for myLink in zPage['links']:
+        masterPageIx += '\t{0}\n'.format( myLink  )
+
+    masterPageIx += '}\n'
+
+    # Crea el archivo 
+    import os 
+    try:
+        os.mkdir( unicode( folder ))
+    except OSError: pass 
+
+    fileNameDot =  '{0}/{1}'.format( folder, 'mapdoc.dot' ) 
+
+    outfile = open(fileNameDot, "w")
+    outfile.write( masterPageIx )
+    outfile.close()
+
+    try:
+        import pygraphviz
+        fileNamePng =  '{0}/{1}'.format( folder, 'mapdoc.png' ) 
+ 
+        graph = pygraphviz.AGraph( fileNameDot )
+        graph.layout( prog= 'dot' )
+        graph.draw( fileNamePng, format ='png')
+ 
+    except ImportError:
+        pass
+
+    import subprocess 
+    subprocess.Popen(["xdot", fileNameDot ])
+
+    # dot -Tpng mapdoc.dot > mapdoc.png
